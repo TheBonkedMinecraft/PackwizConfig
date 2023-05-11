1afbf229108cf7be119a80bc607d01dfdd441cd0
Bhavya Singh <bhavyasingh2611@outlook.com>
2023-05-11 14:05:06 +0530

    hmmm

--- kubejs/server_scripts/script.js
+++ kubejs/server_scripts/script.js
@@ -1,9 +1,5 @@
 // priority: 0
 
-console.info(
-  "Hello, World! (You will see this line every time server resources reload)"
-);
-
 ServerEvents.recipes((event) => {
   let list = [
     "v_slab_compat:betterend/azure_jadestone_vertical_slab",
@@ -38,10 +34,4 @@ ServerEvents.recipes((event) => {
   }
   event.remove({ mod: "mcdw" });
   event.remove({ mod: "mcda" });
-});
-ServerEvents.tags("item", (event) => {
-  // Get the #forge:cobblestone tag collection and add Diamond Ore to it
-  // event.get('forge:cobblestone').add('minecraft:diamond_ore')
-  // Get the #forge:cobblestone tag collection and remove Mossy Cobblestone from it
-  // event.get('forge:cobblestone').remove('minecraft:mossy_cobblestone')
 });
